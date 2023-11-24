3e02850e6df7669f2e0758be42e6fe759ede139d
2azahar786 <2azahar786@gmail.com>
2023-11-25 02:03:17 +0530

    App file injected

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import {useState} from 'react';
+import './App.css';
+import PlayGame from './Components/PlayGame';
+import StartGame from './Components/StartGame';
+
+
+function App() {
+     const [isGameStarted, setIsGameStarted] = useState(false);
+
+     const toggleGamePlay = () =>{
+        setIsGameStarted((prev) => !prev);
+     };
+
+
+      return(
+        <div>
+            {
+                isGameStarted ? (
+                    <PlayGame/>
+                ) :
+                (
+                    <StartGame toggle=  {toggleGamePlay} />
+                )
+            }
+        </div>
+      );
+}
+
+export default App;
